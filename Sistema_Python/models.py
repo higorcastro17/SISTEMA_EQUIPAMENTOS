a7d201cc1c7450fd2a553c63a9bcf2a3d20538ba
higorcastro17 <higorcastro14300302@gmail.com>
2020-01-09 11:59:32 -0300

    Editando arquivos

--- Sistema_Python/models.py
+++ Sistema_Python/models.py
@@ -1,32 +1,27 @@
 from django.db import models
 
 class Equipamento(models.Model):
- mac = models.CharField(
+ MAC = models.CharField(
  primary_key=True, 
  max_length=20,
  null=False,
  blank=False
- )
- sobrenome = models.CharField(
- max_length=255,
- null=False,
- blank=False
- )
- cpf = models.CharField(
- max_length=14,
- null=False,
- blank=False
- )
- tempo_de_servico = models.IntegerField(
- default=0,
- null=False,
- blank=False
- )
- remuneracao = models.DecimalField(
- max_digits=8,
- decimal_places=2,
+)
+
+ Numero_Patrimonio = models.CharField(
+ max_length=50,
  null=False,
  blank=False
  )
 
- objects = models.Manager()
+class Cliente(models.Model):
+ Nome = models.CharField(
+ max_length=15,
+ null=False,
+ 
+ )
+ 
+ 
+
+ 
+objects = models.Manager()
